37992536910b89d818a3199e9670b14e60adb2e4
Conner Iraola <conner_iraola@yahoo.com>
2022-07-06 21:28:08 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-const mongoose = require("mongoose");
-const express = require("express");
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.use(require("./routes"));
-
-mongoose.connect("mongodb://localhost/friends", {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
 
-app.listen(PORT, () => console.log( ` Connected on ${PORT}`));
+
+// const app = express();
+// const PORT = 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(require("./routes"));
+
+// mongoose.connect("mongodb://localhost/friends", {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+// });
+
+// app.listen(PORT, () => console.log( ` Connected on ${PORT}`));
