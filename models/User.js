91a8f4e0ae5f3f1fad59024f1ab1c68559b803ea
Conner Iraola <conner_iraola@yahoo.com>
2022-07-06 21:47:03 -0700

    dependencies

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
-const { Schema, model, Types } = require("mongoose");
-const { thoughtSchema } = require("./Thoughts");
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
     {
@@ -17,8 +16,18 @@ const userSchema = new Schema(
                 /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, 
                 ],
         },
-        thoughts: [thoughtSchema],
-        friends: [this],
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+              },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+              },
+        ],
     },
     {
         toJSON: {
